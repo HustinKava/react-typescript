bd7fa31ced6d6b2f14b632ca578cb12eabc5481e
hkava <hkava@tvo.org>
2022-10-18 08:45:46 -0600

    destructured tsx props

--- src/components/advancedprops/Greet.tsx
+++ src/components/advancedprops/Greet.tsx
@@ -9,12 +9,12 @@ type GreetName = {
 // Props has to have the value of the "GreetName" variable to be validated and useable
 export const Greet = (props: GreetName) => {
   // We can destructure the "messageCount" and assign it a default value of 0
-  const { messageCount = 0 } = props;
+  const { name, isLoggedIn, messageCount = 0 } = props;
   return (
     <div>
-      {props.isLoggedIn ? (
+      {isLoggedIn ? (
         <h2>
-          Welcome {props.name}! You have {messageCount} unread messages
+          Welcome {name}! You have {messageCount} unread messages
         </h2>
       ) : (
         <h2>Welcome Guest</h2>
